b3f5d3988b933cc8bfdc5a62b639b8b641f85e50
Андрей <andrydo2009@yandex.ru>
2023-02-08 23:09:29 +0300

    Ok?

--- src/CourseWorkTask/CourseFunction.java
+++ src/CourseWorkTask/CourseFunction.java
@@ -1,6 +1,9 @@
 package CourseWorkTask;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 
 public class CourseFunction {
 
@@ -12,41 +15,33 @@ public class CourseFunction {
         System.out.println ( g );
         String[] words = g.split ( divider );
         System.out.println ( "Количество слов в строке - " + words.length );
-//        int j = 1;
-//        for (String word : words) {
-//            System.out.println ( j++ + " " + word );
-//        }
     }
 
     public static void searchWords(String s) {
-//        System.out.println ( s );
+
         String k = s.replace ( "." , "" );
         String g = k.replace ( "," , "" );
         System.out.println ( g );
         String divider = " ";
         String[] words = g.split ( divider );
-        Map<String, Integer> map = new HashMap<> ();
-        for (String word : words) {
-            if (!map.containsKey ( word )) {
-                map.put ( word , 1 );
-            } else {
-                map.put ( word , map.get ( word ) + 1 );
-            }
-        }
-//        map.forEach ( (key , value) -> System.out.println ( "Слово <" + key + "> повторяется " + value + " раз " ) );
+        Map<String, Integer> map = Arrays.stream( words )
+                .map ( String::toLowerCase )
+                .collect( Collectors.toMap ( Function.identity(), value->1, (a, b) -> a + 1));
         System.out.println ( "TOP слов: " );
         map.entrySet ().stream ()
-                .sorted ( Map.Entry.comparingByValue ( Comparator.reverseOrder () ) )
-                .forEach ( stringIntegerEntry -> {
+                .sorted (Map.Entry.comparingByValue ( Comparator.reverseOrder () ) )
+                    .forEach ( stringIntegerEntry -> {
+
                     System.out.println ( stringIntegerEntry.getKey () + " - " + stringIntegerEntry.getValue () );
                 } );
+
+
     }
 
     public static void main(String[] args) {
 
-    /* String testString = "За свою карьеру я пропустил более 9000 бросков, проиграл почти 300 игр.
-    26 раз мне доверяли сделать финальный победный бросок, и я промахивался.
-    Я терпел поражения снова, и снова, и снова. И именно поэтому я добился успеха."*/
+    /* String testString = "За свою карьеру я пропустил более 9000 бросков, проиграл почти 300 игр. 26 раз мне доверяли сделать финальный победный бросок, и я промахивался.Я терпел поражения снова, и снова, и снова. И именно поэтому я добился успеха.
+    "*/
 
         Scanner scanner = new Scanner ( System.in );
         System.out.print ( "Введите строку: " );
@@ -57,5 +52,3 @@ public class CourseFunction {
 
     }
 }
-
-// end
